b0338bad2f41fa7819e3dd8aa468a50bc8aa381f
Matthias Kahlert <m-kahlert@web.de>
2024-12-19 16:06:10 +0100

    Test which looks for a button with dynamic id

--- selenium-tests/tests/test_dynamic_id.py
+++ selenium-tests/tests/test_dynamic_id.py
@@ -1,1 +1,14 @@
 # file intended for test dynamic id
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_dynamic_id():
+    driver = webdriver.Chrome()
+    driver.get("http://uitestingplayground.com/dynamicid")
+    
+    # Suche einen Button mit dynamischer ID
+    button = driver.find_element(By.XPATH, "//button[contains(text(),'Button')]")
+    assert button is not None, "Button nicht gefunden"
+    button.click()
+    driver.quit()
