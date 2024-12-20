6ee7fc2a437fdb074a52ca83413e95891a55c1fa
Matthias Kahlert <m-kahlert@web.de>
2024-12-20 22:21:02 +0100

    usage of selenium manager and usage of google-chrome.stable

--- selenium-tests/tests/test_dynamic_id.py
+++ selenium-tests/tests/test_dynamic_id.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 def test_dynamic_id():
@@ -11,15 +12,25 @@ def test_dynamic_id():
     chrome_options.add_argument("--no-sandbox")  # Sandbox deaktivation
     chrome_options.add_argument("--disable-dev-shm-usage")  # Shared memory usage deaktivation
 
+    # Selenium Manager wird automatisch den passenden Chromedriver finden
+    driver = webdriver.Chrome(options=chrome_options)
+
     # WebDriver creation
-    service = Service()  # Optional, falls du einen spezifischen Pfad für den ChromeDriver benötigst
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+  #   service = Service()  # Optional, falls du einen spezifischen Pfad für den ChromeDriver benötigst
+   #  driver = webdriver.Chrome(service=service, options=chrome_options)
 	
 	# Testlogik
-    driver.get("http://uitestingplayground.com/dynamicid")
-    
-    # Look for a Button with a dynamic ID
-    button = driver.find_element(By.XPATH, "//button[contains(text(),'Button')]")
-    assert button is not None, "Button nicht gefunden"
-    button.click()
-    driver.quit()
+	try: 
+		driver.get("http://uitestingplayground.com/dynamicid")
+		
+		# Look for a Button with a dynamic ID
+		button = driver.find_element(By.XPATH, "//button[contains(text(),'Button')]")
+		assert button is not None, "Button nicht gefunden"
+		button.click()
+		# Optional: Validierung nach dem Klick (wenn notwendig)
+        # e.g., prüfen, ob eine neue Seite geladen wurde
+        # new_element = driver.find_element(By.ID, "expectedElementId")
+        # assert new_element is not None, "Expected element not found after click"
+	finally:
+		# Teardown: Driver schließen, selbst bei Fehlern
+		driver.quit()
