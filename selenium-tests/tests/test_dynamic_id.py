d42f80f2553488d0a2a7997c811ff725701a43ba
Matthias Kahlert <m-kahlert@web.de>
2024-12-20 12:50:01 +0100

    Fixing Selenium tests for CI environment, adding headless mode to chrome options, installing browser and webdriver in github actions

--- selenium-tests/tests/test_dynamic_id.py
+++ selenium-tests/tests/test_dynamic_id.py
@@ -1,13 +1,24 @@
 # file intended for test dynamic id
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 def test_dynamic_id():
-    driver = webdriver.Chrome()
+	# Set Chrome optionen
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Headless mode
+    chrome_options.add_argument("--no-sandbox")  # Sandbox deaktivation
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Shared memory usage deaktivation
+
+    # WebDriver creation
+    service = Service()  # Optional, falls du einen spezifischen Pfad für den ChromeDriver benötigst
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+	
+	# Testlogik
     driver.get("http://uitestingplayground.com/dynamicid")
     
-    # Suche einen Button mit dynamischer ID
+    # Look for a Button with a dynamic ID
     button = driver.find_element(By.XPATH, "//button[contains(text(),'Button')]")
     assert button is not None, "Button nicht gefunden"
     button.click()
